4d8dde4afcd0a27ae22792e3460f73bacfcf7fce
Damhee Kim <ekal.kdh@gmail.com>
2022-04-13 23:07:33 +0900

    adding productToAdd to cartItems

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,7 +1,18 @@
 import { createContext, useState } from "react";
 
+// if the product already exists, then add quantity or add product to cart Items
 const addCartItem = (cartItems, productToAdd) => {
-
+    // find if cartItems contains productToAdd
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === productToAdd.id);
+    // if found, increment quantity
+    if(existingCartItem) {
+        return cartItems.map((cartItem) => cartItem.id === productToAdd.id 
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        );
+    }
+    // return new array with modified cartItems with new cart item
+    return [...cartItems, {...productToAdd, quantity: 1}]
 }
 
 export const CartContext = createContext({
@@ -15,11 +26,12 @@ export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
 
+    // whenever user clicks add to cart, then add product
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
     
-    const value = { isCartOpen, setIsCartOpen };
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
